55a6943f66cfe5931b83cfb9a437f534328575f0
gbordapoo <104753709+gbordapoo@users.noreply.github.com>
2024-12-09 14:10:48 -0800

    Update user and password section

--- streamlit.py
+++ streamlit.py
@@ -13,6 +13,7 @@ import streamlit as st
 import os
 from PIL import Image
 import unicodedata
+import hmac
 
 # App configuration
 st.set_page_config(page_title='Performance Field - Logos Scatter Plot',
@@ -48,6 +49,45 @@ def resize_logos(logos_path, output_path, size=(50, 50)):
 
 # Normalize logos
 resize_logos("./logos", "./normalized_logos", size=(50, 50))
+
+# Authentication
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
 
 # Add the app logo to the top of the sidebar
 logo_placeholder = st.sidebar.empty()
